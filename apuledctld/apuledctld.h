7e7810b2c1b31acb4e0f23add392cf84396e5e61
Alexey Prokopchuk <alexpro.ewr@gmail.com>
2018-06-20 03:27:52 +0300

    Added sysctl apuled description checking in check_apuled_devs()

--- apuledctld/apuledctld.h
+++ apuledctld/apuledctld.h
@@ -1,6 +1,12 @@
 #ifndef __APULEDCTLD_H_
 #define __APULEDCTLD_H_
 
+#define SYSCTL_APULED_DESC "dev.apuled.0.%desc"
+#define APU_LED1 "/dev/led/led1"
+#define APU_LED2 "/dev/led/led2"
+#define APU_LED3 "/dev/led/led3"
+#define APU_MODESW "/dev/modesw"
+
 int check_apuled_module();
 int check_apuled_devs();
 
--- apuledctld/common.cpp
+++ apuledctld/common.cpp
@@ -4,6 +4,8 @@
 #include <sys/param.h>
 #include <sys/linker.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/sysctl.h>
 
 #include "apuledctld.h"
 
@@ -23,14 +25,28 @@ int check_apuled_module()
 int check_apuled_devs()
 {
     struct stat st;
+    char* desc;
+    size_t dsz=0;
 
-    if(stat("/dev/led/led1",&st)) return -1;
+    //Get buffer size first
+    sysctlbyname(SYSCTL_APULED_DESC,NULL,&dsz,NULL,0);
+    if(dsz<=0) return -1;
+    desc=new char[dsz];
+    if(sysctlbyname(SYSCTL_APULED_DESC,desc,&dsz,NULL,0))
+    {
+_serr:
+	delete []desc;
+	return -1;
+    }
+    if(strstr(desc,"APU")!=desc) goto _serr; //Not APU<x>
+    delete []desc;
+    if(stat(APU_LED1,&st)) return -1;
     if(!S_ISCHR(st.st_mode)) return -1;
-    if(stat("/dev/led/led2",&st)) return -1;
+    if(stat(APU_LED2,&st)) return -1;
     if(!S_ISCHR(st.st_mode)) return -1;
-    if(stat("/dev/led/led3",&st)) return -1;
+    if(stat(APU_LED3,&st)) return -1;
     if(!S_ISCHR(st.st_mode)) return -1;
-    if(stat("/dev/modesw",&st)) return -1;
+    if(stat(APU_MODESW,&st)) return -1;
     if(!S_ISCHR(st.st_mode)) return -1;
     return 0;
 }
